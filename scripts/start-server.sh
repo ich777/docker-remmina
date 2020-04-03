febcc5eb1226202574e5a4cb1aa17f6048ba70d9
ich777 <ich777@noreply.example.org>
2020-04-03 13:31:30 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,7 +4,24 @@ export $XDG_DATA_HOME=${DATA_DIR}
 export DISPLAY=:99
 
 echo "---Preparing Server---"
+echo "---Checking for old logfiles---"
+find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
+find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+echo "---Checking for old lock files---"
+find /tmp -name ".X99*" -exec rm -f {} \;
 chmod -R ${DATA_PERM} ${DATA_DIR}
+
+echo "---Starting Xvfb server---"
+screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
+sleep 2
+
+echo "---Starting x11vnc server---"
+screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+sleep 2
+
+echo "---Starting noVNC server---"
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
+sleep 2
 
 echo "---Sleep zZz---"
 sleep infinity
